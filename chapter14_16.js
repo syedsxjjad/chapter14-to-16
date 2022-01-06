43cafcc81cd5a8b867bc0cabd27864f65f4f3d0d
syedsxjjad <syedsxjjad@gmail.com>
2022-01-06 20:54:44 +0500

    14 to 16

---
+++ chapter14_16.js
@@ -0,0 +1,160 @@
+//////-------Arrays------///////
+
+
+
+
+//_________1___________//
+// var student=[];
+
+
+//_________3___________//
+// var student=["Syed"];
+
+
+//_________4___________//
+// var student=[2];
+
+
+//_________5___________//
+// var student=[true];
+
+
+//_________6___________//
+// var student=["Syed",2,"ss",5];
+
+
+//_________7___________//
+// var qualification=["SSC","WW","RR","QQ","AA","PP"];
+// for (let i= 0; i < qualification.length; i++) {
+//     document.write(`<br>${qualification[i]}`);       
+// }
+
+
+//_________8___________//
+// var student=["Syed","Salman","Sajid"];
+// var score=[300,400,350];
+
+// var totalMarks=500;
+// for (let x = 0; x < student.length; x++) {
+//     document.write(`Score of ${student[x]} is ${score[x]}. Percentage: ${(score[x]/totalMarks)*100} <br/>`);    
+// }
+
+
+//_________9___________//
+
+//a)Add the element from the Begning;
+
+// var color =["Red","Green","Blue","Black"];
+//  var user=prompt("Chose The color");
+//   color.unshift(user);
+//   document.write(color);
+
+
+
+//b)Add the element from the End;
+
+//  var color =["Red","Green","Blue","Black"];
+//  var user=prompt("Chose The color"); 
+//   color.push(user);
+//   document.write(color);
+
+
+//c)Add the element from the Begning;
+
+// var color =["Red","Green","Blue","Black"];
+// var user=prompt("Chose The First color");
+// var user1=prompt("Chose The Second color");
+//  color.unshift(user,user1);
+//  document.write(color);
+
+
+
+//d)Add the element from the End;
+
+//  var color =["Red","Green","Blue","Black"];
+//   color.shift(color);
+//   document.write(color);
+
+
+//e)Remove the element from the End;
+
+//  var color =["Red","Green","Blue","Black"];
+//   color.pop(color);
+//   document.write(color);
+
+
+
+//f)Add the element want to user;
+
+//  var color =["Red","Green","Blue","Black"];
+//  var ind=prompt("Enter index");
+//  var user=prompt("Enter the Color");
+//   color.splice(ind,0,user);
+//   document.write(color);
+
+
+//g)delete the element want to user;
+
+//  var color =["Red","Green","Blue","Black"];
+//  var ind=prompt("Enter Delete index");
+//    var ss=color.splice(0,ind,ind);
+//   document.write(ss);
+
+
+
+
+
+//_________10___________//
+
+// const sc=["s","w","q","t","a"];
+//  for (let i = 0; i < 1; i++) {
+//     sc.sort();        
+//     document.write(`score of student ${sc}<br/>`)
+// }    
+
+
+
+
+
+//_________11___________//
+
+// var cities=["Karachi","Islamabad","Quetta","Peshawar","Rawalpindi"];
+//  var scities=cities.slice(0,3);
+//  console.log(scities);
+//  document.write(scities)
+
+
+//_________12___________//
+
+// var arr=["This","is","my","pen"];
+//  for (let x = 0; x < 1; x++) {
+//      var jn=arr.join('  ');
+//     document.write(jn); 
+// }
+    
+
+//_________13___________//
+
+// var dev = [];
+// for (let e = 0; e < 4; e++) {
+//     const abc = prompt();
+//     dev.push(abc)
+// }
+// document.write(`Device:${dev} <br> `);
+// dev.reverse()
+// document.write(`out:${dev[0]} <br> out:${dev[1]} <br> out:${dev[2]} <br>
+// out:${dev[3]} <br>`); 
+
+
+
+//_________14___________//
+
+// var dev = [];
+// for (let e = 0; e < 4; e++) {
+//     const abc = prompt();
+//     dev.push(abc)
+// }
+// document.write(`Device:${dev} <br> `);
+// dev.reverse()
+// document.write(`out:${dev[0]} <br> out:${dev[1]} <br> out:${dev[2]} <br>
+// out:${dev[3]} <br>`); 
